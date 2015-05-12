6bf322cc927e8fc893ec9c388141de0ba8a85a2a
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2015-05-13 08:46:48 +0900

    add timestamp

--- js/mail.js
+++ js/mail.js
@@ -18,14 +18,37 @@ function loadStorage(){
       sendMail(api,email);
     }
   });
-
 }
+
+/**
+ * 日付をフォーマットする
+ * @param  {Date}   date     日付
+ * @param  {String} [format] フォーマット
+ * @return {String}          フォーマット済み日付
+ */
+var formatDate = function (date, format) {
+  if (!format) format = 'YYYY-MM-DD hh:mm:ss.SSS';
+  format = format.replace(/YYYY/g, date.getFullYear());
+  format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+  format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2));
+  format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2));
+  format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+  format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+  if (format.match(/S/g)) {
+    var milliSeconds = ('00' + date.getMilliseconds()).slice(-3);
+    var length = format.match(/S/g).length;
+    for (var i = 0; i < length; i++) format = format.replace(/S/, milliSeconds.substring(i, i + 1));
+  }
+  return format;
+};
+
 function sendMail(api,email){
   var txtVal = $('#message').val();
   if(txtVal.length == 0)  return;
   txtVal = txtVal.replace(/\r\n/g, "<br />");
   txtVal = txtVal.replace(/(\n|\r)/g, "<br />");
   var subject = txtVal.split( '<br />' );
+  txtVal += '<br />---<br />Send from Papio at ' + formatDate(new Date(),'YYYY/MM/DD hh:mm:ss') + '<br />';
   console.log(txtVal);
 
   $.ajax({
